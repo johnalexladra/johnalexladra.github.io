c89970a0bc1d67503641b05d6549af44c5ca0b7c
John Alex Ladra <johnalexladra@gmail.com>
2019-12-20 02:40:54 +0800

    Fix GithubCalendar bug + make it responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "johnalexladra");
+    GitHubCalendar(".calendar", "johnalexladra", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
